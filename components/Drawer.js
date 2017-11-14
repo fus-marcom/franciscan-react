de7400bd942223d1a79fe4548d3c303391172f98
Jesse R Weigel <jesse@jesseweigel.com>
2017-11-14 15:11:18 -0500

    Add search and nested list to drawer

--- components/Drawer.js
+++ components/Drawer.js
@@ -3,47 +3,105 @@ import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import Drawer from 'material-ui/Drawer'
 import List, { ListItem, ListItemText } from 'material-ui/List'
+import Collapse from 'material-ui/transitions/Collapse'
+import ExpandLess from 'material-ui-icons/ExpandLess'
+import ExpandMore from 'material-ui-icons/ExpandMore'
+import SearchIcon from 'material-ui-icons/Search'
+import IconButton from 'material-ui/IconButton'
 import Divider from 'material-ui/Divider'
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input'
+import { FormControl } from 'material-ui/Form'
 
-const styles = {
+const styles = theme => ({
   list: {
     width: 250
   },
   listFull: {
     width: 'auto'
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 4
+  },
+  formControl: {
+    margin: theme.spacing.unit
   }
-}
+})
 
-const TemporaryDrawer = props => {
-  const { classes, toggleDrawer, open } = props
+class TemporaryDrawer extends React.Component {
+  state = {
+    openItems: true,
+    search: ''
+  }
 
-  return (
-    <Drawer open={open} onRequestClose={toggleDrawer}>
-      <div
-        tabIndex={0}
-        role="button"
-        onClick={toggleDrawer}
-        onKeyDown={toggleDrawer}
-      >
-        <div className={classes.list}>
-          <List>
-            <ListItem>
-              <img
-                className="drawerImg"
-                width="200"
-                src="/static/img/fus-logo.svg"
-                alt="logo"
-              />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={'Item'} inset />
-            </ListItem>
-          </List>
-          <Divider />
+  handleClick = () => {
+    this.setState({ openItems: !this.state.openItems })
+  }
+
+  handleSearchChange = e => {
+    this.setState({ search: e.target.value })
+  }
+
+  render () {
+    const { classes, toggleDrawer, open } = this.props
+
+    return (
+      <Drawer open={open} onRequestClose={toggleDrawer}>
+        <div tabIndex={0} role="button">
+          <div className={classes.list}>
+            <List>
+              <ListItem>
+                <img
+                  className="drawerImg"
+                  width="200"
+                  src="/static/img/fus-logo.svg"
+                  alt="logo"
+                />
+              </ListItem>
+              <ListItem>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor="search">Search</InputLabel>
+                  <Input
+                    id="search"
+                    type="text"
+                    value={this.state.search}
+                    onChange={this.handleSearchChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton>
+                          <SearchIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </ListItem>
+              <ListItem button onClick={this.handleClick}>
+                <ListItemText inset primary="Inbox" />
+                {this.state.openItems ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse
+                component="li"
+                in={this.state.openItems}
+                transitionDuration="auto"
+                unmountOnExit
+              >
+                <List
+                  disablePadding
+                  onClick={toggleDrawer}
+                  onKeyDown={toggleDrawer}
+                >
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary="Starred" />
+                  </ListItem>
+                </List>
+              </Collapse>
+            </List>
+            <Divider />
+          </div>
         </div>
-      </div>
-    </Drawer>
-  )
+      </Drawer>
+    )
+  }
 }
 
 TemporaryDrawer.propTypes = {
