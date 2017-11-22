dd21c0a16012dd13318515d600798544dd3e3f16
Jesse R Weigel <jesse@jesseweigel.com>
2017-11-22 15:11:10 -0500

    Add custom server

---
+++ pages/faculty.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import withRoot from '../components/withRoot'
+
+export class Faculty extends React.Component {
+  static getInitialProps ({ query: { id } }) {
+    return { id }
+  }
+
+  render () {
+    return (
+      <Layout>
+        <h1>My {this.props.id} blog post</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </Layout>
+    )
+  }
+}
+
+export default withRoot(Faculty)
