fe13f9e48974c03207898f8f26f1db4fcf613d69
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-01-20 22:02:50 +0100

    test: see if tests run on commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,39 +1,39 @@
-import React from 'react'
-import { withStyles } from 'material-ui/styles'
-import Grid from 'material-ui/Grid'
-import Typography from 'material-ui/Typography'
-import classNames from 'classnames'
+import React from "react";
+import { withStyles } from "material-ui/styles";
+import Grid from "material-ui/Grid";
+import Typography from "material-ui/Typography";
+import classNames from "classnames";
 
 const styles = theme => ({
   root: {
     marginTop: 30,
     backgroundColor: `${theme.palette.primary[500]}`,
-    borderTop: 'solid 3px #998643',
-    paddingTop: '16px',
-    overflowX: 'hidden'
+    borderTop: "solid 3px #998643",
+    paddingTop: "16px",
+    overflowX: "hidden"
   },
   footerSections: {
-    margin: '0 16px'
+    margin: "0 16px"
   },
   subFooter: {
-    backgroundColor: 'rgba(0, 0, 0, 0.15)',
-    padding: '8px 16px 8px 16px',
-    marginTop: '8px'
+    backgroundColor: "rgba(0, 0, 0, 0.15)",
+    padding: "8px 16px 8px 16px",
+    marginTop: "8px"
   },
   footerText: {
-    color: '#fff',
-    fontSize: '18px',
+    color: "#fff",
+    fontSize: "18px",
     lineHeight: 1.5
   },
   white: {
-    color: '#ffffff'
+    color: "#ffffff"
   }
-})
+});
 
 class Footer extends React.Component {
-  render () {
-    const { classes } = this.props
-    const currentYear = new Date().getFullYear()
+  render() {
+    const { classes } = this.props;
+    const currentYear = new Date().getFullYear();
     return (
       <div className={classes.root}>
         <Grid container spacing={0}>
@@ -41,7 +41,7 @@ class Footer extends React.Component {
             <Typography
               className={classNames(classes.footerText, classes.footerSections)}
             >
-              Bacon ipsum dolor amet rump leberkas doner, andouille tenderloin
+              Test ipsum dolor amet rump leberkas doner, andouille tenderloin
               beef ribs ham shankle kielbasa drumstick tail brisket. Beef ribs
               kielbasa venison, chicken sausage bresaola turkey tongue andouille
               pork belly meatball. Bresaola sausage t-bone frankfurter. Pig
@@ -86,15 +86,15 @@ class Footer extends React.Component {
             <Typography
               className={classes.white}
               type="subheading"
-              component={'span'}
+              component={"span"}
             >
               © {currentYear} Franciscan University of Steubenville
             </Typography>
           </Grid>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
-export default withStyles(styles)(Footer)
+export default withStyles(styles)(Footer);
