040f726652a5dcc3b48dc80796594a6cb9f85676
Jesse R Weigel <jesse@jesseweigel.com>
2018-02-08 15:31:24 -0500

    chore: upgrade to next 5.0.1-canary.2
    re #77

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  webpack: function (c) {
-    if (c.resolve.alias) {
-      delete c.resolve.alias['react']
-      delete c.resolve.alias['react-dom']
-    }
-    return c
-  }
-}
