317bf17fe77dbdae4afa9f97b9d7111a67a183e9
Jesse R Weigel <jesse@jesseweigel.com>
2018-03-01 13:36:08 -0500

    feat: style search page

--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react'
+import { withStyles } from 'material-ui/styles'
 import Layout from '../components/Layout'
 import Head from 'next/head'
 import withRoot from '../components/withRoot'
 import { getJSON } from '../utils/fetch'
 import debounce from 'lodash.debounce'
 
+const styles = theme => ({
+  root: {
+    '& br': {
+      display: 'unset'
+    }
+  },
+  contentContainer: {
+    width: '100%',
+    maxWidth: '70%',
+    margin: '0 auto',
+    [theme.breakpoints.down('md')]: {
+      maxWidth: '85%'
+    },
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '95%'
+    }
+  },
+  searchResult: {
+    margin: '16px 0'
+  },
+  title: {
+    fontSize: '24px',
+    fontWeight: 400,
+    display: 'block'
+  },
+  type: {
+    fontSize: '18px',
+    fontWeight: 300,
+    textTransform: 'uppercase'
+  }
+})
+
 class Page extends Component {
+  state = {
+    data: []
+  }
   /**
    * Make api call based on searchTerm
    * Render cards from api data
    */
   fetchSearchTerm = searchTerm => {
     const apiUrl = 'http://104.236.41.59/wp-json/wp/v2/'
-    const params = `multiple-post-type?search=${searchTerm}&type[]=post&type[]=page&type[]=admissions-page&type[]=chapel-page`
+    const params = `multiple-post-type?per_page=100&search=${searchTerm}&type[]=post&type[]=page&type[]=admissions-page&type[]=chapel-page`
     getJSON(apiUrl + params).then(data => this.setState({ data }))
     console.log(this.state.data)
   }
@@ -82,6 +118,7 @@ class Page extends Component {
   }
 
   render () {
+    const { classes } = this.props
     return (
       <Layout>
         <Head>
@@ -91,29 +128,36 @@ class Page extends Component {
             type="text/css"
           />
         </Head>
-
-        <form onSubmit={this.formGetResults}>
-          <input
-            id="search"
-            name="search"
-            onChange={this.getSearchResults}
-            type="search"
-            style={{ width: '100%', paddingLeft: '4px' }}
-          />
-          <label htmlFor="search">Search</label>
-          <svg fill="#fff" height="24" viewBox="0 0 24 24" width="24">
-            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </svg>
-        </form>
-        {this.state.data
-          ? this.state.data.map(item => (
-            <div key={item.id}>{item.title.rendered}</div>
-          ))
-          : null}
+        <div className={classes.contentContainer}>
+          <form onSubmit={this.formGetResults}>
+            <input
+              id="search"
+              name="search"
+              onChange={this.getSearchResults}
+              type="search"
+              style={{ width: '100%', paddingLeft: '4px' }}
+            />
+            <label htmlFor="search">Search</label>
+            <svg fill="#fff" height="24" viewBox="0 0 24 24" width="24">
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+          </form>
+          {this.state.data
+            ? this.state.data.map(item => (
+              <div key={item.id} className={classes.searchResult}>
+                <span className={classes.type}>{item.type}</span>
+                <span
+                  className={classes.title}
+                  dangerouslySetInnerHTML={{ __html: item.title.rendered }}
+                />
+              </div>
+            ))
+            : null}
+        </div>
       </Layout>
     )
   }
 }
 
-export default withRoot(Page)
+export default withRoot(withStyles(styles)(Page))
