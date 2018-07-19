78ef3053066a137347c5a83407c6c43012358270
Christopher Brown <chbphone55@gmail.com>
2018-07-18 20:23:35 -0600

    Use object-fit to make all cards and images the same size

--- components/FacultyListItem.js
+++ components/FacultyListItem.js
@@ -10,27 +10,41 @@ import Paper from '@material-ui/core/Paper'
 
 const styles = theme => ({
   card: {
+    height: '250px',
     [theme.breakpoints.down('xl')]: {
       maxWidth: '600px'
     }
   },
   cardContent: {
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    height: '100%',
+    width: '100%'
   },
-  media: {
-    minHeight: '280px',
-    [theme.breakpoints.up('xl')]: {
-      minHeight: '1366px'
-    },
-    [theme.breakpoints.up('lg')]: {
-      minHeight: 500
-    }
-  },
+  // media: {
+  //   minHeight: '280px',
+  //   [theme.breakpoints.up('xl')]: {
+  //     minHeight: '1366px'
+  //   },
+  //   [theme.breakpoints.up('lg')]: {
+  //     minHeight: 500
+  //   }
+  // },
   quote: {
     fontSize: '16px',
     paddingLeft: '8px',
     borderLeft: '4px solid rgba(0, 0, 0, 0.24)'
+  },
+  portraitWrapper: {
+    height: '100%',
+    width: '50%',
+    overflow: 'hidden'
+  },
+  portrait: {
+    objectFit: 'cover',
+    objectPosition: 'top',
+    width: '100%',
+    height: '100% !important'
   }
 })
 
@@ -41,7 +55,9 @@ class FacultyListItem extends Component {
     return (
       <Grid item xs={12} sm={9} md={6} lg={4} xl={3} className={classes.card}>
         <Paper className={classes.cardContent}>
-          <CardContent>
+          <CardContent
+            style={{ width: imageObj ? 'calc(50% - 48px)' : 'auto' }}
+          >
             <Typography variant="headline" component="h2">
               {profileName}
             </Typography>
@@ -62,7 +78,15 @@ class FacultyListItem extends Component {
               </CardActions>
             )}
           </CardContent>
-          {imageObj && <img src={imageObj.sourceUrl} alt={imageObj.altText} />}
+          {imageObj && (
+            <div className={classes.portraitWrapper}>
+              <img
+                src={imageObj.sourceUrl}
+                alt={imageObj.altText}
+                className={classes.portrait}
+              />
+            </div>
+          )}
         </Paper>
       </Grid>
     )
