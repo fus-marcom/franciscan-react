6ee44e12cda1fa870ba76a451df9b1b1c715ae54
Jesse R Weigel <jesse@jesseweigel.com>
2018-08-13 16:27:20 -0400

    feat: add connection to salesforce api

--- server.js
+++ server.js
@@ -7,6 +7,48 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dir: '.', dev })
 const handle = app.getRequestHandler()
 
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+
+console.log(username)
+
+const jsforce = require('jsforce')
+const conn = new jsforce.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  loginUrl: 'https://fus.my.salesforce.com/'
+})
+conn.login(username, password, function (err, userInfo) {
+  if (err) {
+    return console.error(err)
+  }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
+  console.log(conn.accessToken)
+  console.log(conn.instanceUrl)
+  // logged in user property
+  console.log('User ID: ' + userInfo.id)
+  console.log('Org ID: ' + userInfo.organizationId)
+  // ...
+
+  // Multiple records creation
+  conn
+    .sobject('Account')
+    .create([{ Name: 'My Account #1' }, { Name: 'My Account #2' }], function (
+      err,
+      rets
+    ) {
+      if (err) {
+        return console.error(err)
+      }
+      for (var i = 0; i < rets.length; i++) {
+        if (rets[i].success) {
+          console.log('Created record id : ' + rets[i].id)
+        }
+      }
+      // ...
+    })
+})
+
 // TODO: split this object up into three: one for for routes with 1 param, 2params, and 3 params. Then change routing function to use the correct obj depending on the num of params
 const translationObj = {
   faculty: { page: '/faculty', type: 'faculty' },
